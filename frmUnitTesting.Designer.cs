4928f38a4da1ead6d31e9c58646c07c40314c87d
niyagriff200 <130501997+niyagriff200@users.noreply.github.com>
2026-03-03 08:36:20 -0600

    Add project files.

---
+++ frmUnitTesting.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Unit_Testing
+{
+    partial class frmUnitTesting
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // frmUnitTesting
+            // 
+            AccessibleRole = AccessibleRole.MenuBar;
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1326, 815);
+            Name = "frmUnitTesting";
+            Text = "Unit Testing Form";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ frmUnitTesting.cs
@@ -0,0 +1,10 @@
+namespace Unit_Testing
+{
+    public partial class frmUnitTesting : Form
+    {
+        public frmUnitTesting()
+        {
+            InitializeComponent();
+        }
+    }
+}
