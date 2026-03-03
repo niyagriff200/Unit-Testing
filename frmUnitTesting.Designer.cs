013a99c0f2ead11b85af8039db9596b8d8024b86
niyagriff200 <130501997+niyagriff200@users.noreply.github.com>
2026-03-03 09:15:26 -0600

    Added 2 bool functions and button functionability

--- frmUnitTesting.Designer.cs
+++ frmUnitTesting.Designer.cs
@@ -28,7 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tb_ConfirmPassword = new TextBox();
+            tb_NewPassword = new TextBox();
+            lbl_AgeTitle = new Label();
+            lbl_NewPassword = new Label();
+            lbl_ConfirmPassword = new Label();
+            btn_AgeConfirm = new Button();
+            btn_PasswordConfirm = new Button();
+            num_AgeValue = new NumericUpDown();
+            lbl_IsAdult = new Label();
+            lbl_PasswordConfirmed = new Label();
+            ((System.ComponentModel.ISupportInitialize)num_AgeValue).BeginInit();
             SuspendLayout();
+            // 
+            // tb_ConfirmPassword
+            // 
+            tb_ConfirmPassword.Location = new Point(460, 508);
+            tb_ConfirmPassword.Name = "tb_ConfirmPassword";
+            tb_ConfirmPassword.Size = new Size(250, 47);
+            tb_ConfirmPassword.TabIndex = 1;
+            // 
+            // tb_NewPassword
+            // 
+            tb_NewPassword.Location = new Point(132, 508);
+            tb_NewPassword.Name = "tb_NewPassword";
+            tb_NewPassword.Size = new Size(250, 47);
+            tb_NewPassword.TabIndex = 2;
+            // 
+            // lbl_AgeTitle
+            // 
+            lbl_AgeTitle.AutoSize = true;
+            lbl_AgeTitle.Location = new Point(132, 94);
+            lbl_AgeTitle.Name = "lbl_AgeTitle";
+            lbl_AgeTitle.Size = new Size(257, 41);
+            lbl_AgeTitle.TabIndex = 3;
+            lbl_AgeTitle.Text = "What is Your Age?";
+            // 
+            // lbl_NewPassword
+            // 
+            lbl_NewPassword.AutoSize = true;
+            lbl_NewPassword.Location = new Point(132, 431);
+            lbl_NewPassword.Name = "lbl_NewPassword";
+            lbl_NewPassword.Size = new Size(211, 41);
+            lbl_NewPassword.TabIndex = 4;
+            lbl_NewPassword.Text = "New Password";
+            // 
+            // lbl_ConfirmPassword
+            // 
+            lbl_ConfirmPassword.AutoSize = true;
+            lbl_ConfirmPassword.Location = new Point(460, 431);
+            lbl_ConfirmPassword.Name = "lbl_ConfirmPassword";
+            lbl_ConfirmPassword.Size = new Size(257, 41);
+            lbl_ConfirmPassword.TabIndex = 5;
+            lbl_ConfirmPassword.Text = "Confirm Password";
+            // 
+            // btn_AgeConfirm
+            // 
+            btn_AgeConfirm.Location = new Point(411, 167);
+            btn_AgeConfirm.Name = "btn_AgeConfirm";
+            btn_AgeConfirm.Size = new Size(188, 58);
+            btn_AgeConfirm.TabIndex = 6;
+            btn_AgeConfirm.Text = "Confirm";
+            btn_AgeConfirm.UseVisualStyleBackColor = true;
+            btn_AgeConfirm.Click += btn_AgeConfirm_Click;
+            // 
+            // btn_PasswordConfirm
+            // 
+            btn_PasswordConfirm.Location = new Point(781, 502);
+            btn_PasswordConfirm.Name = "btn_PasswordConfirm";
+            btn_PasswordConfirm.Size = new Size(188, 58);
+            btn_PasswordConfirm.TabIndex = 7;
+            btn_PasswordConfirm.Text = "Confirm";
+            btn_PasswordConfirm.UseVisualStyleBackColor = true;
+            btn_PasswordConfirm.Click += btn_PasswordConfirm_Click;
+            // 
+            // num_AgeValue
+            // 
+            num_AgeValue.Location = new Point(89, 174);
+            num_AgeValue.Name = "num_AgeValue";
+            num_AgeValue.Size = new Size(300, 47);
+            num_AgeValue.TabIndex = 8;
+            // 
+            // lbl_IsAdult
+            // 
+            lbl_IsAdult.AutoSize = true;
+            lbl_IsAdult.Location = new Point(89, 280);
+            lbl_IsAdult.Name = "lbl_IsAdult";
+            lbl_IsAdult.Size = new Size(0, 41);
+            lbl_IsAdult.TabIndex = 9;
+            // 
+            // lbl_PasswordConfirmed
+            // 
+            lbl_PasswordConfirmed.AutoSize = true;
+            lbl_PasswordConfirmed.Location = new Point(132, 617);
+            lbl_PasswordConfirmed.Name = "lbl_PasswordConfirmed";
+            lbl_PasswordConfirmed.Size = new Size(0, 41);
+            lbl_PasswordConfirmed.TabIndex = 10;
             // 
             // frmUnitTesting
             // 
@@ -36,11 +131,33 @@
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1326, 815);
+            Controls.Add(lbl_PasswordConfirmed);
+            Controls.Add(lbl_IsAdult);
+            Controls.Add(num_AgeValue);
+            Controls.Add(btn_PasswordConfirm);
+            Controls.Add(btn_AgeConfirm);
+            Controls.Add(lbl_ConfirmPassword);
+            Controls.Add(lbl_NewPassword);
+            Controls.Add(lbl_AgeTitle);
+            Controls.Add(tb_NewPassword);
+            Controls.Add(tb_ConfirmPassword);
             Name = "frmUnitTesting";
             Text = "Unit Testing Form";
+            ((System.ComponentModel.ISupportInitialize)num_AgeValue).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private TextBox tb_ConfirmPassword;
+        private TextBox tb_NewPassword;
+        private Label lbl_AgeTitle;
+        private Label lbl_NewPassword;
+        private Label lbl_ConfirmPassword;
+        private Button btn_AgeConfirm;
+        private Button btn_PasswordConfirm;
+        private NumericUpDown num_AgeValue;
+        private Label lbl_IsAdult;
+        private Label lbl_PasswordConfirmed;
     }
 }
--- frmUnitTesting.cs
+++ frmUnitTesting.cs
@@ -1,10 +1,75 @@
+using System.Diagnostics;
+
 namespace Unit_Testing
 {
     public partial class frmUnitTesting : Form
     {
+
+
         public frmUnitTesting()
         {
             InitializeComponent();
+        }
+
+        // Method checks whether or not the strings are matching
+        private bool IsMatching(string text1, string text2)
+        {
+            if (text1 == text2) //if they match return true, if not return false
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Method that checks if the user is an adult
+        private bool IsAdult(int value)
+        {
+            if (value >= 18) //if 18 or older, return true, if not return false
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btn_AgeConfirm_Click(object sender, EventArgs e)
+        {
+            int age = (int)num_AgeValue.Value; //grab the value from the numUpDown
+
+            Debug.Assert(IsAdult(age) == true, "Expected: Age should be 18 or older.");
+
+            if (IsAdult(age))
+            {
+                lbl_IsAdult.Text = "You're an adult. Please continue.";
+            }
+            else
+            {
+                lbl_IsAdult.Text = "Adults only!!!";
+            }
+        }
+
+        private void btn_PasswordConfirm_Click(object sender, EventArgs e)
+        {
+            string newPassword = tb_NewPassword.Text;
+            string confirmPassword = tb_ConfirmPassword.Text;
+
+            Debug.Assert(IsMatching(newPassword, confirmPassword) == true, "Expected: newPassword and confirmPassword should be matching.");
+
+            if (IsMatching(newPassword, confirmPassword))
+            {
+                lbl_PasswordConfirmed.Text = "Password correct.";
+                lbl_PasswordConfirmed.ForeColor = Color.Green;
+            }
+            else
+            {
+                lbl_PasswordConfirmed.Text = "Password incorrect.";
+                lbl_PasswordConfirmed.ForeColor = Color.Red;
+            }
         }
     }
 }
